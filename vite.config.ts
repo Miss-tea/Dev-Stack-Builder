a25a2d8096e4a25b6a1f454e3421b12afe720902
Miss-tea <lubainasakhawat@gmail.com>
2026-09-13 23:39:41 +0600

    finally

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/Dev-Stack-Builder/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
